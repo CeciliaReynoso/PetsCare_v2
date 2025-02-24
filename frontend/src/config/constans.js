637d35d88f73b452dc7c713140903742f1a7a06b
ceciliareynoso <cecilia.reynosom@outlook.com>
2025-02-24 12:17:48 -0500

    Corregir la ruta en el endpoint de tienda para asegurar la correcta formación de la URL

--- frontend/src/config/constans.js
+++ frontend/src/config/constans.js
@@ -16,7 +16,7 @@ export const ENDPOINT = {
   productsFilter:`${URLBASE}/productos/filtros`,
   product: `${URLBASE}/productos/:id`, 
   promocion: `${URLBASE}/productos/promocion`,
-  tienda: `${URLBASE}productos/tienda`,
+  tienda: `${URLBASE}/productos/tienda`,
 
   // Endpoints privados
   users: `${URLBASE}/usuarios`,
