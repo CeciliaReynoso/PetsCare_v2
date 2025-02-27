3646ca4277ebeb44e21abcbe4e9013317598258e
ceciliareynoso <cecilia.reynosom@outlook.com>
2025-02-27 17:45:29 -0500

    Eliminar clase innecesaria en HomePerfil.jsx

--- frontend/src/views/HomePerfil.jsx
+++ frontend/src/views/HomePerfil.jsx
@@ -40,7 +40,7 @@ const HomePerfil = () => {
   }
 
   return (
-    <div className='home-perfil-container fix-container'>
+    <div className='home-perfil-container'>
       <h3>Mi Perfil</h3>
       <p>Datos registrados</p>
       <p>Nombre: {userNombre}</p>
