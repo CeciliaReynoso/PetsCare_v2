7d39302ae920a4a3f0b92bf55396a6c3e709ebc5
ceciliareynoso <cecilia.reynosom@outlook.com>
2025-02-27 15:02:49 -0500

    Actualizar lógica en HomePerfil: comentar uso de useAuth y RolesContext para simplificar el acceso a datos del perfil

--- frontend/src/views/HomePerfil.jsx
+++ frontend/src/views/HomePerfil.jsx
@@ -3,37 +3,37 @@ import RolesContext from '../context/RolesContext';
 import useAuth from '../hooks/useAuth';
 
 const HomePerfil = () => {
-  const { user } = useAuth();
-  const { cargo, setCargo } = useContext(RolesContext);
+  // const { user } = useAuth();
+  // const { cargo, setCargo } = useContext(RolesContext);
 
   
-  useEffect((user) => {
-    if (!user) {
-      return;
-    }
-    // if (user.rol !== 'COMPRADOR' && user.rol !== 'ADMINISTRADOR' && user.rol !== 'CLIENTE') {
+  // useEffect((user) => {
+  //   if (!user) {
+  //     return;
+  //   }
+  //   // if (user.rol !== 'COMPRADOR' && user.rol !== 'ADMINISTRADOR' && user.rol !== 'CLIENTE') {
     //   navigate('/');
     //   return;
     // }
-    if (user) {
+    // if (user) {
       const userNombre = window.sessionStorage.getItem('userNombre');
       const userRol = window.sessionStorage.getItem('userRol');
       const userEmail = window.sessionStorage.getItem('userEmail');
       const userDireccion = window.sessionStorage.getItem('userDireccion');
       
-      setCargo({ nombre: userNombre, rol: userRol, email: userEmail, direccion: userDireccion });
-    }
-  }, [user, setCargo]);
+  //     setCargo({ nombre: userNombre, rol: userRol, email: userEmail, direccion: userDireccion });
+  //   }
+  // }, [user, setCargo]);
 
   return (
     <div className='home-perfil-container'>
       <div>
       <h3>Mi Perfil</h3>
       <p>Datos registrados</p>
-      <p>Nombre: {user?.nombre}</p>
-      <p>Rol: {user?.rol}</p>
-      <p>Dirección: {user?.direccion}</p>
-      <p>Email: {user?.email}</p>
+      <p>Nombre: {userNombre}</p>
+      <p>Rol: {userRol}</p>
+      <p>Dirección: {userDireccion}</p>
+      <p>Email: {userEmail}</p>
       </div>
     </div>
   );
