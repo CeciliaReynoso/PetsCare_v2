be9c188ad75d1eb7726e98005352a89fd9bd9589
ceciliareynoso <cecilia.reynosom@outlook.com>
2025-02-27 15:18:57 -0500

    Actualizar lógica en HomePerfil: agregar estado para almacenar y mostrar información del usuario desde sessionStorage

--- frontend/src/views/HomePerfil.jsx
+++ frontend/src/views/HomePerfil.jsx
@@ -1,40 +1,40 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import RolesContext from '../context/RolesContext';
 import useAuth from '../hooks/useAuth';
 
 const HomePerfil = () => {
-const { user } = useAuth();
-const { cargo, setCargo } = useContext(RolesContext);
+  const { user } = useAuth();
+  const { cargo, setCargo } = useContext(RolesContext);
+  const [userNombre, setUserNombre] = useState('');
+  const [userRol, setUserRol] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userDireccion, setUserDireccion] = useState('');
 
-  
-  useEffect((user) => {
-  //   if (!user) {
-  //     return;
-  //   }
-  //   // if (user.rol !== 'COMPRADOR' && user.rol !== 'ADMINISTRADOR' && user.rol !== 'CLIENTE') {
-    //   navigate('/');
-    //   return;
-    // }
+  useEffect(() => {
     if (user) {
       const userNombre = window.sessionStorage.getItem('userNombre');
       const userRol = window.sessionStorage.getItem('userRol');
       const userEmail = window.sessionStorage.getItem('userEmail');
       const userDireccion = window.sessionStorage.getItem('userDireccion');
-      
-  setCargo({ nombre: userNombre, rol: userRol, email: userEmail, direccion: userDireccion });
-  //   }
+
+      setCargo({ nombre: userNombre, rol: userRol, email: userEmail, direccion: userDireccion });
+
+      setUserNombre(userNombre);
+      setUserRol(userRol);
+      setUserEmail(userEmail);
+      setUserDireccion(userDireccion);
     }
-  }, [user]);
+  }, [user, setCargo]);
 
   return (
     <div className='home-perfil-container'>
       <div>
-      <h3>Mi Perfil</h3>
-      <p>Datos registrados</p>
-      <p>Nombre: {userNombre}</p>
-      <p>Rol: {userRol}</p>
-      <p>Dirección: {userDireccion}</p>
-      <p>Email: {userEmail}</p>
+        <h3>Mi Perfil</h3>
+        <p>Datos registrados</p>
+        <p>Nombre: {userNombre}</p>
+        <p>Rol: {userRol}</p>
+        <p>Dirección: {userDireccion}</p>
+        <p>Email: {userEmail}</p>
       </div>
     </div>
   );
